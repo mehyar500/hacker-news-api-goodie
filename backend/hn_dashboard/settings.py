fbf869ed5665e787bea8ec37e46c359791522944
Mehyar Swelim <mehyar500@gmail.com>
2025-07-08 12:17:24 -0400

    Refactor settings.py to use environment variables for SECRET_KEY, DEBUG, and ALLOWED_HOSTS

--- backend/hn_dashboard/settings.py
+++ backend/hn_dashboard/settings.py
@@ -10,9 +10,11 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
-# Build paths inside the project like this: BASE_DIR / 'subdir'.
+load_dotenv(dotenv_path=Path(__file__).parent.parent / '.env')
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 
@@ -20,12 +22,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-#&dsdq#+7yit-)sa%gd!b$ke4tmj4r1s9qno%a0o4u#4j)5!ui'
+SECRET_KEY = os.getenv("SECRET_KEY", default="django-insecure-#&dsdq#+7yit-)sa%gd!b$ke4tmj4r1s9qno%a0o4u#4j)5!ui")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = os.getenv("DEBUG", default="True") == "True"
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", default="localhost,127.0.0.1").split(",")]
 
 
 # Application definition
